509ec776dddc25edb4a976ad88e068177ee82e9a
Endosmile <kolarovmunari@gmail.com>
2026-07-31 17:19:17 +0100

    Delete backend directory

--- backend/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { search } = require('../controllers/searchController');
-const { authenticate } = require('../middleware/auth');
-
-router.get('/', authenticate, search);
-
-module.exports = router;
